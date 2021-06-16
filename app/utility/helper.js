8c70c132e9ee9a82ab139478c05d67abe9aadf08
gautamb97 <www.gautam971997@gmail.com>
2021-06-16 16:26:46 +0530

    refactor: validation using joi

---
+++ app/utility/helper.js
@@ -0,0 +1,39 @@
+/**
+ * @description   : It is use to validate the inputs we are getting from client side using joi and
+ *                  also using Regular expression to follow the pattern properly.
+ * @package       : joi
+ * @file          : helper.js
+ * @author        : Gautam Biswal <gautam971997@gmail.com>
+*/
+const Joi = require('joi');
+
+/**
+ * @description   : validating all parameters we are getting from the user for registration
+ * @method        : string, min, required, pattern of JOI
+*/
+const authSchema = Joi.object({
+  firstName: Joi.string()
+    .min(3)
+    .required()
+    .pattern(new RegExp('^[A-Z][a-z]{3,}$')),
+
+  lastName: Joi.string()
+    .min(3)
+    .required()
+    .pattern(new RegExp('^[A-Z][a-z]{3,}$')),
+
+  email: Joi.string()
+    .pattern(new RegExp('([a-z0-9\\.-]+)@([a-z0-9-]+).([a-z]{2,4})(.[a-z]{2})?$'))
+    .required(),
+
+  password: Joi.string()
+    .required()
+    .pattern(new RegExp('(?=.*[A-Z])(?=.*[0-9])(?=.*\\W)[a-zA-Z0-9\\#]{8,}')),
+
+  role: Joi.string()
+    .required()
+});
+
+module.exports = {
+    authSchema
+}
