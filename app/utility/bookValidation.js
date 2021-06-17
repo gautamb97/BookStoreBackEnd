75d06ca0959037bc9c7a94ea2b59d469d2e5705e
gautamb97 <www.gautam971997@gmail.com>
2021-06-17 18:29:37 +0530

    refactor: validation for update book property

--- app/utility/bookValidation.js
+++ app/utility/bookValidation.js
@@ -35,6 +35,31 @@ class BookHelper {
         .required()
   });
 
+  updateBookProperty = Joi.object({
+    author: Joi.string()
+      .min(3)
+      .required()
+      .pattern(new RegExp('^[A-Za-z ]{3,}$')),
+
+    title: Joi.string()
+      .min(3)
+      .required(),
+
+    image: Joi.string()
+      .required(),
+
+    quantity: Joi.number()
+      .required(),
+
+    price: Joi.number()
+      .required(),
+
+    description: Joi.string()
+        .required(),
+    
+    bookId: Joi.string()
+        .required(),
+  });
 }
 
 module.exports = new BookHelper();
