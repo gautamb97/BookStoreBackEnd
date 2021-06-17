19fa9a49e709c763588acefbe43dce65feebb45e
gautamb97 <www.gautam971997@gmail.com>
2021-06-17 14:39:23 +0530

    refactor: route name change for user and admin registration

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,15 +1,15 @@
 /**
  * @description   : It is use to route the APIs
- * @file          : user.js
+ * @file          : routes.js
  * @author        : Gautam Biswal <gautam971997@gmail.com>
 */
 const controller = require('../controller/registration');
 const helper = require('../utility/helper');
 module.exports = (app) => {
 
-    app.post('/user/registration', helper.setRole('user'), controller.register);
+    app.post('/userRegistration', helper.setRole('user'), controller.register);
 
-    app.post('/admin/registration', helper.setRole('admin'), controller.register);
+    app.post('/adminRegistration', helper.setRole('admin'), controller.register);
 
     app.post('/login', controller.login);
 }
