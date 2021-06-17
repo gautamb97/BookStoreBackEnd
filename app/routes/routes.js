331cd64fb45abee3b5ffd9245d7f334ce8437c9d
gautamb97 <www.gautam971997@gmail.com>
2021-06-17 17:45:54 +0530

    refactor: route for add book in bookstore

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -4,6 +4,7 @@
  * @author        : Gautam Biswal <gautam971997@gmail.com>
 */
 const controller = require('../controller/registration');
+const booksController = require('../controller/books')
 const helper = require('../utility/helper');
 module.exports = (app) => {
 
@@ -16,4 +17,6 @@ module.exports = (app) => {
     app.post('/forgotPassword', controller.forgotPassword);
 
     app.post('/resetPassword', helper.verifyToken, controller.resetPassword);
+
+    app.post('/books', helper.verifyRole, booksController.addBook)
 }
